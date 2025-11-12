ebc424fed0cb7b5c88a5c8893b9084c255999600
Krishnanand <krishnanandsreekarthika@gmail.com>
2025-11-12 19:51:26 +0530

    axios
    use axios to fetch data from backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,24 @@
+import axios from 'axios'
 import Header from '../components/Header'
 import { products} from '../assets/data/products'
 import './HomePage.css'
 
 function HomePage() {
 
-    fetch('http://localhost:3000/api/products')
+    //using fetch to get data from backend API
+    // fetch('http://localhost:3000/api/products')
+    // .then((response)=>{
+    //      return response.json();
+    // }).then((data)=>{
+    //             console.log(data)
+    //         });
+
+
+    //using axios to get data from backend API
+    axios.get('http://localhost:3000/api/products')
     .then((response)=>{
-         return response.json();
-    }).then((data)=>{
-                console.log(data)
-            });
+        console.log(response.data);
+    });
 
     return (
         <>
