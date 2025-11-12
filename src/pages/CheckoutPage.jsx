8df1dca79e4a20f0ab1ef615160f9d78752b54c1
Krishnanand <krishnanandsreekarthika@gmail.com>
2025-11-12 14:42:06 +0530

    orders page & tracking page

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,6 +1,7 @@
 import './CheckoutPage.css'
 import './Checkout-header.css'
 
+
 function CheckoutPage(){
     return(
     <>
@@ -9,7 +10,7 @@ function CheckoutPage(){
         <div className="checkout-header">
             <div className="header-content">
             <div className="checkout-header-left-section">
-                <a href="index.html">
+                <a href="/">
                 <img className="logo" src="images/logo.png" />
                 <img className="mobile-logo" src="images/mobile-logo.png" />
                 </a>
@@ -17,7 +18,7 @@ function CheckoutPage(){
 
             <div className="checkout-header-middle-section">
                 Checkout (<a className="return-to-home-link"
-                href="index.html">3 items</a>)
+                href="/">3 items</a>)
             </div>
 
             <div className="checkout-header-right-section">
--- src/pages/OrdersPage.jsx
+++ src/pages/OrdersPage.jsx
@@ -1,10 +1,13 @@
+import './OrdersPage.css'
+import './Header.css'
+
 function OrdersPage() {
     return (
         <>
 
             <div className="header">
                 <div className="left-section">
-                    <a href="index.html" className="header-link">
+                    <a href="/" className="header-link">
                         <img className="logo"
                             src="images/logo-white.png" />
                         <img className="mobile-logo"
@@ -21,12 +24,12 @@ function OrdersPage() {
                 </div>
 
                 <div className="right-section">
-                    <a className="orders-link header-link" href="orders.html">
+                    <a className="orders-link header-link" href="/orders">
 
                         <span className="orders-text">Orders</span>
                     </a>
 
-                    <a className="cart-link header-link" href="checkout.html">
+                    <a className="cart-link header-link" href="/checkout">
                         <img className="cart-icon" src="images/icons/cart-icon.png" />
                         <div className="cart-quantity">3</div>
                         <div className="cart-text">Cart</div>
@@ -80,7 +83,7 @@ function OrdersPage() {
                             </div>
 
                             <div className="product-actions">
-                                <a href="tracking.html">
+                                <a href="/tracking">
                                     <button className="track-package-button button-secondary">
                                         Track package
                                     </button>
@@ -108,7 +111,7 @@ function OrdersPage() {
                             </div>
 
                             <div className="product-actions">
-                                <a href="tracking.html">
+                                <a href="/tracking">
                                     <button className="track-package-button button-secondary">
                                         Track package
                                     </button>
@@ -159,7 +162,7 @@ function OrdersPage() {
                             </div>
 
                             <div className="product-actions">
-                                <a href="tracking.html">
+                                <a href="/tracking">
                                     <button className="track-package-button button-secondary">
                                         Track package
                                     </button>
