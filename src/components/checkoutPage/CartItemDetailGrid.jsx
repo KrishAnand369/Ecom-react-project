e84235d3964e4faa4196ba20870ee3d2e6df5c68
Krishnanand <krishnanandsreekarthika@gmail.com>
2025-11-14 13:46:20 +0530

    update quantity in cart using checkbox and used onKeyDown method

--- src/components/checkoutPage/CartItemDetailGrid.jsx
+++ src/components/checkoutPage/CartItemDetailGrid.jsx
@@ -1,10 +1,44 @@
 import axios  from 'axios';
 import { formatMoney } from '../../utils/money'
 import DeliveryOptions from './DeliveryOptions';
+import { useState } from 'react';
 const CartItemDetailGrid = ({cartItem,deliveryOptions,fetchCartItems}) => {
+    const[isUpdatingQuantity,SetIsUpdatingQuantity]= useState(false);
+    const[quantity,SetQuantity]= useState(cartItem.quantity);
+
     const deleteCartItem = async ()=>{
        await axios.delete(`/api/cart-items/${cartItem.product.id}`);
        await fetchCartItems();
+    };
+    const updateCartItem = async ()=>{
+       await axios.put(`/api/cart-items/${cartItem.product.id}`,{
+                quantity: parseInt(quantity)
+            })
+       await fetchCartItems();
+    };
+    const updateQuantity =  ()=>{
+        if(isUpdatingQuantity){
+            SetIsUpdatingQuantity(false)
+            updateCartItem();
+        } else{
+            SetIsUpdatingQuantity(true)
+        } 
+    };
+
+    const handleQuantityKeyDown= (event)=>{
+
+        const keyPressed = event.key;
+        if(keyPressed==='Enter'){
+            updateQuantity();
+        } else if(keyPressed==='Escape'){
+            SetIsUpdatingQuantity(false);
+        } else if (event.key === 'ArrowUp') {
+        // Decrease count on Up arrow key
+        SetQuantity(quantity => quantity - 1);
+      } else if (event.key === 'ArrowDown') {
+        // Increase count on Down arrow key
+        SetQuantity(quantity => quantity + 1);
+      }
     };
     
     return (
@@ -21,14 +55,25 @@ const CartItemDetailGrid = ({cartItem,deliveryOptions,fetchCartItems}) => {
                 </div>
                 <div className="product-quantity">
                     <span>
-                        Quantity: <span className="quantity-label">{cartItem.quantity}</span>
-                    </span>
-                    <span className="update-quantity-link link-primary">
+                        Quantity: 
+                        {isUpdatingQuantity?
+                        <input type='text' className='quantity-textbox' value={quantity}
+                        onChange={(e)=>{
+                            SetQuantity(e.target.value);
+                        }}
+                        onKeyDown={handleQuantityKeyDown}
+                        style={{opacity:isUpdatingQuantity?1:0}}
+                        />
+                        :
+                    <span className="quantity-label" style={{opacity:isUpdatingQuantity?0:1}}> {cartItem.quantity}</span>
+                    } </span> 
+                    <span className="update-quantity-link link-primary"
+                    onClick={updateQuantity}>
                         Update
                     </span>
                     <span className="delete-quantity-link link-primary"
                         onClick={deleteCartItem}>
-                        Delete
+                        Remove
                     </span>
                 </div>
             </div>
